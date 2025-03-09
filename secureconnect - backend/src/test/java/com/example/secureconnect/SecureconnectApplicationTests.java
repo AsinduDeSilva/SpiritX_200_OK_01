3aece3e67733c84c98542b6377ead3e0b9334ed3
Asindu De Silva <asindudesilva@gmail.com>
2025-03-09 09:08:36 +0530

    backend init

---
+++ secureconnect - backend/src/test/java/com/example/secureconnect/SecureconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secureconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
